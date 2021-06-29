c22ae3dc33913d3d55130fc55dad279d76b117c0
Eazy21 <ericrconklin@gmail.com>
2021-06-28 18:15:34 -0600

    Final changes and small formatting changes

--- code/Challenge_1_Eric_Conklin.py
+++ code/Challenge_1_Eric_Conklin.py
@@ -1,6 +1,5 @@
 #Challenge 1 Columbia Fintech
 #Eric Conklin
-# coding: utf-8
 
 import csv
 from pathlib import Path
@@ -20,18 +19,21 @@ loan_costs = [500, 600, 200, 1000, 450]
 # How many loans are in the list?
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
+
 number_of_loans = len(loan_costs)
 print(f"The number of loans is {number_of_loans}")
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
+
 total_loan_value = sum(loan_costs)
 print(f"The total loan value is ${total_loan_value}")
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
+
 average_loan_amount = total_loan_value/number_of_loans
 print(f"The average loan amount is ${average_loan_amount}")
 
@@ -68,6 +70,7 @@ loan = {
 
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
+
 future_value = loan.get("future_value")
 remaining_months = loan.get("remaining_months")
 print(f"The future value of the loan is ${future_value}")
@@ -77,9 +80,10 @@ print(f"There are {remaining_months} remaining months on the loan")
 # Use a minimum required return of 20% as the discount rate.
 #   You'll want to use the **monthly** version of the present value formula.
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
+
 discount_rate = 0.2
 present_value = future_value / (1 + discount_rate/12) ** remaining_months
-print(f"present value is ${round(present_value,2)}")
+print(f"The present value of the loan is ${round(present_value,2)}")
 
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
@@ -119,7 +123,7 @@ new_loan = {
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
 def calc_present_value(future_value,remaining_months,annual_discount_rate):
-    present_value = future_value / (1 + discount_rate/12) ** remaining_months
+    present_value = future_value / (1 + annual_discount_rate/12) ** remaining_months
     return present_value    
 
 
@@ -127,7 +131,7 @@ def calc_present_value(future_value,remaining_months,annual_discount_rate):
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 annual_discount_rate = 0.2
 present_value = calc_present_value(new_loan["future_value"],new_loan["remaining_months"],annual_discount_rate)
-print(f"The present value of the loan is: {round(present_value,2)}")
+print(f"The present value of the loan is ${round(present_value,2)}")
 
 
 """Part 4: Conditionally filter lists of loans.
@@ -202,7 +206,6 @@ header = ["loan_price", "remaining_months", "repayment_interval", "future_value"
 
 # Set the output file path
 output_path = Path("inexpensive_loans.csv")
-print(output_path)
 
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
